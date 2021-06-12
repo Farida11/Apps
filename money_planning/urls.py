bfde8e3bc8c9fe2c73fbbded47fd52229a7e2a31
Farida <saituna@mail.ru>
2021-06-12 21:38:16 +0300

    Add some sort option

--- money_planning/urls.py
+++ money_planning/urls.py
@@ -2,15 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index, name = "index"),
-   path('users', views.users, name = "users"),
-   path('transactions', views.transactions, name = "transactions"),
-   path('create_user', views.create_user, name = "create_user"),
-   path('create_transact', views.create_transact, name = "create_transact"),
-   path('user/<int:pk>', views.user_view, name = "user_view"),
-   path('transaction/<int:pk>', views.transact_view, name = "transact_view"),
-   path('edit_user/<int:pk>', views.edit_user, name = "edit_user"),
-   path('edit_transact/<int:pk>', views.edit_transact, name = "edit_transact"),
-   path('delete_user/<int:pk>', views.delete_user, name = "delete_user"),
-   path('delete_transac/<int:pk>', views.delete_transact, name = "delete_transact"),
+   path('', views.index, name="index"),
+   path('sort_filter/<str:tag>', views.sort_filter, name="sort_filter"),
+   path('users', views.users, name="users"),
+   path('transactions', views.transactions, name="transactions"),
+   path('create_user', views.create_user, name="create_user"),
+   path('create_transact', views.create_transact, name="create_transact"),
+   path('user/<int:pk>', views.user_view, name="user_view"),
+   path('transaction/<int:pk>', views.transact_view, name="transact_view"),
+   path('edit_user/<int:pk>', views.edit_user, name="edit_user"),
+   path('edit_transact/<int:pk>', views.edit_transact, name="edit_transact"),
+   path('delete_user/<int:pk>', views.delete_user, name="delete_user"),
+   path('delete_transac/<int:pk>', views.delete_transact, name="delete_transact"),
 ]
--- money_planning/views.py
+++ money_planning/views.py
@@ -1,19 +1,52 @@
 from django.shortcuts import render
-from .models import Users,Transactions
-from django.http import HttpResponseRedirect, HttpResponseNotFound
+from .models import Users, Transactions
+from django.http import HttpResponseRedirect, HttpResponseNotFound, HttpResponse
 from .forms import UsersForm, TransactionsForm
+from datetime import datetime
+
 
 from rest_framework import viewsets
-from rest_framework import permissions
+# from rest_framework import permissions
 from .serializers import UsersSerializer, TransactionsSerializer
+
 
 def index(request):
     list_of_users = Users.objects.all()
+    list_of_transact = Transactions.objects.all()
+    s = []
+    for user in list_of_users:
+        for trans in list_of_transact:
+            if trans.user_id.id == user.id:
+                s.append({"Id": user.id, "Name": user.first_name,
+                          "Last_Name": user.last_name,
+                          "Email_address": user.email,
+                          "Date": trans.date.strftime("%Y-%m-%d"),
+                          "Sum": trans.amount})
     template = 'index.html'
-    context={
-    'list_of_users' : list_of_users
+    context = {
+        'group_and_filter': True,
+        'show': False,
+        'two_model': s
     }
     return render(request, template, context)
+
+
+def sort_filter(request, tag=" "):
+    list_of_transact = Transactions.objects.all()
+    if tag == "date":
+        list_of_transact = Transactions.objects.all().order_by('date')
+    if tag == "income":
+        list_of_transact = Transactions.objects.all().order_by('amount')
+    if tag == "outcome":
+        list_of_transact = Transactions.objects.all().order_by('-amount')
+    template = 'index.html'
+    context = {
+        'group_and_filter': False,
+        "show": True,
+        'list_of_transact': list_of_transact,
+    }
+    return render(request, template, context)
+
 
 # -------------for swagger ------------#
 class UsersViewSet(viewsets.ModelViewSet):
@@ -22,6 +55,7 @@ class UsersViewSet(viewsets.ModelViewSet):
     """
     queryset = Users.objects.all()
     serializer_class = UsersSerializer
+
 
 class TransactionsViewSet(viewsets.ModelViewSet):
     """
@@ -32,29 +66,35 @@ class TransactionsViewSet(viewsets.ModelViewSet):
 
 # -------------for swagger ------------#
 
+
 # show all users
 def users(request):
     list_of_users = Users.objects.all()
     template = 'Users.html'
-    context={
-    'list_of_users' : list_of_users
+    context = {
+        'group_and_fillter': False,
+        'list_of_users': list_of_users
     }
     return render(request, template, context)
+
 
 # show user
 def user_view(request, pk):
     get_user = Users.objects.get(id=pk)
     template = 'user_view.html'
-    context={
-    'list_of_users' : get_user
+    context = {
+        'group_and_fillter': False,
+        'get_user': get_user
     }
     return render(request, template, context)
 
-#create new user
+
+# create new user
 def create_user(request):
     template = 'create_user.html'
-    context={
-          'form':UsersForm()
+    context = {
+          'group_and_fillter': False,
+          'form': UsersForm()
     }
     if request.method == "POST":
         user = UsersForm(request.POST)
@@ -73,8 +113,9 @@ def edit_user(request, pk):
     try:
         person = Users.objects.get(id=pk)
         template = 'edit_user.html'
-        context={
-              'form':UsersForm(instance=person)
+        context = {
+              'group_and_fillter': False,
+              'form': UsersForm(instance=person)
         }
         if request.method == "POST":
             user = UsersForm(request.POST, instance=person)
@@ -88,38 +129,45 @@ def edit_user(request, pk):
     except Users.DoesNotExist:
         return HttpResponseNotFound("<h2>User not found</h2>")
 
+
 # delete user in database
 def delete_user(request, pk):
     try:
-        user =  Users.objects.get(id=pk)
+        user = Users.objects.get(id=pk)
         user.delete()
         return HttpResponseRedirect("/users")
     except Users.DoesNotExist:
         return HttpResponseNotFound("<h2> User not found</h2>")
 
+
 # show all transactions
 def transactions(request):
-    list_of_transact= Transactions.objects.all()
+    list_of_transact = Transactions.objects.all()
     template = 'Transactions.html'
-    context={
-    'list_of_transact' : list_of_transact
+    context = {
+        'group_and_fillter': False,
+        'list_of_transact': list_of_transact
     }
     return render(request, template, context)
+
 
 # show transaction
 def transact_view(request, pk):
     get_transact = Transactions.objects.get(id=pk)
     template = 'transact_view.html'
-    context={
-    'list_of_users' : get_transact
+    context = {
+        'group_and_fillter': False,
+        'get_transact': get_transact
     }
     return render(request, template, context)
+
 
 # create new transaction
 def create_transact(request):
     template = 'create_transact.html'
-    context={
-          'form':TransactionsForm()
+    context = {
+          'group_and_fillter': False,
+          'form': TransactionsForm()
     }
     if request.method == "POST":
         transact = TransactionsForm(request.POST)
@@ -132,13 +180,15 @@ def create_transact(request):
 
     return render(request, template, context)
 
+
 # edit information about transaction
 def edit_transact(request, pk):
     try:
         trans = Transactions.objects.get(id=pk)
         template = 'edit_transact.html'
-        context={
-              'form':TransactionsForm(instance=trans)
+        context = {
+              'group_and_fillter': False,
+              'form': TransactionsForm(instance=trans)
         }
         if request.method == "POST":
             transact = TransactionsForm(request.POST, instance=trans)
@@ -151,6 +201,7 @@ def edit_transact(request, pk):
             return render(request, template, context)
     except Users.DoesNotExist:
         return HttpResponseNotFound("<h2>Transactions not found</h2>")
+
 
 # delete transaction in database
 def delete_transact(request, pk):
